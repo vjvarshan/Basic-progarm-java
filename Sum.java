e547290100ba5a96c2cbee74cc966075340a8405
vjvarshan <vjvarshan1912@gmail.com>
2026-01-22 18:17:06 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -49,5 +49,27 @@ public class Sum {
         System.out.println(sum);
     }
 }
+// Sum of Prime Numbers
+public class Sum {
+   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    System.out.println("Sum of Prime Numbers  :");
+    int sum = 0;
+    for(int i=1;i<=a;i++){
+        int count = 0;
 
+        for(int j=1;j<=i;j++){
+        if(i%j==0) {
+            count++;
+        }
+      } 
+      if(count == 2){
+        sum = sum + i;
+      }
+    }
+     System.out.println(sum);
+ }
+
+}
 
