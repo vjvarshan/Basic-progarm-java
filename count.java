003a0be04370163754decb6765871772e1f2effc
vjvarshan <vjvarshan1912@gmail.com>
2026-01-22 17:58:18 +0530

    Update count.java

--- count.java
+++ count.java
@@ -68,3 +68,27 @@ public class count {
         System.out.println(count);
     }
 }
+
+//count prime Numbers
+
+import java.util.Scanner;
+
+public class count {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int Primecount = 0;
+        for(int i=1;i<=a;i++){
+            int count = 0;
+           for(int j=1;j<=i;j++){
+            if(i%j==0){
+                count++;
+            }
+           }
+           if(count==2){
+            Primecount++;
+           }
+        }
+       System.out.println(Primecount); 
+    }
+}
